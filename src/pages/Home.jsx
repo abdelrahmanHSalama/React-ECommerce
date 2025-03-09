820c1c06053008ce53d9e21ea527c54ead04bb17
Abdelrahman Salama <3bdlrhmnHSalama@gmail.com>
2025-03-10 01:40:11 +0200

    v1.5

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import ProductCard from "../components/ProductCard";
 import productBanner from "../assets/productBanner.png";
+import productBannerMobile from "../assets/productBanner-mobile.png";
 import LoadingSpinner from "../components/LoadingSpinner";
 
 const Home = () => {
@@ -16,11 +17,17 @@ const Home = () => {
 
     return (
         <main className="p-2">
-            <img
-                className="w-full object-cover"
-                src={productBanner}
-                alt="Product Banner"
-            />
+            <picture>
+                <source
+                    srcSet={productBannerMobile}
+                    media="(max-width: 640px)"
+                />
+                <img
+                    className="w-full object-cover"
+                    src={productBanner}
+                    alt="Product Banner"
+                />
+            </picture>
 
             <div className="mt-5 flex flex-wrap justify-around gap-y-2">
                 {products.slice(0, 5).map((product) => (
